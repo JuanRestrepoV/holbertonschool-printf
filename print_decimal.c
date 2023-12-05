050fcd03deca31821c8c512e36cc9b48fecaba07
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-05 13:10:40 -0800

    C - printf, print_integer and print_decimal functions

---
+++ print_decimal.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <unistd.h>
+/**
+* print_decimal - Fucntion that print decimal numbers using write function.
+* @arguments: va_list type variable.
+*
+* Description: this function receive a va_list 'arguments' parameter.
+* Return: integer.
+*/
+int print_decimal(va_list arguments);
+{
+}
---
+++ print_integer.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <unistd.h>
+/**
+* print_integer - Fucntion that print a integer number using write function.
+* @arguments: va_list type variable.
+*
+* Description: this function receive a va_list 'arguments' parameter.
+* Return: integer.
+*/
+int print_integer(va_list arguments);
+{
+}

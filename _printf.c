8e9751194da333f565238381485b81ba2e0b41de
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-05 12:55:34 -0800

    C - printf, print_string and _printf functions

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/*
+* _printf - Custom printf function
+* @format: String containing format specifiers
+*
+* This function prints formatted output to stdout according to the
+* provided format string.
+*
+* Return: Number of characters printed (excluding the null byte),
+* or -1 in case of error.
+*/
+int _printf(const char *format, ...)
+{
+}

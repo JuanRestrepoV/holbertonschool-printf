1133b0576ccc03a9e15b32133aaa074cffed4347
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-06 09:34:08 -0800

    Printf function calls to each specifier

--- _printf.c
+++ _printf.c
@@ -13,4 +13,47 @@
 */
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int count = 0;
+	va_list arguments;
+
+	va_start(arguments, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				count += (print_char(arguments));
+			}
+			if (format[i] == 's')
+			{
+				count += (print_string(arguments));
+			}
+			if (format[i] == 'd')
+			{
+				count += (print_decimal(arguments));
+			}
+			if (format[i] == 'i')
+			{
+				count += (print_integer(arguments));
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format, 1);
+				return (2);
+			}
+		}
+		else
+		{
+			write (1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(arguments);
+	return (count);
 }

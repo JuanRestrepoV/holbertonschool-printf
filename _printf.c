c1710a5ac76b7dbecd0037f65202fafdabc66178
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-06 18:10:25 -0800

    compilation error uipdate

--- _printf.c
+++ _printf.c
@@ -20,7 +20,7 @@ int _printf(const char *format, ...)
 
 	va_start(arguments, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == NULL))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 	{
 		return (-1);
 	}
